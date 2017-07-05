d5916a300f060025d47594947b97226844eff4c5
Jain <Rajesh.Jain@aristocrat.com>
2017-07-05 20:19:57 +0530

    commit

---
+++ app/products/product.service.js
@@ -0,0 +1,91 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+/*
+In this class we are basically declaring the service which can be used in the product component anywhere.Similarly we can
+declaire the any other service such that it can be used across the product.Services can be such as logging service or
+any other service where data need to be shared across different components.
+
+Here in ant component we can use these service by importing this class and then using it in that class
+with
+
+myservice m = new myservice();
+but this will create the individual object for every component and will data will not be shared across components.
+
+Anuglar provides the way out by defining the function which is @injectable() which hands this service to anguar and
+tells it that create a single object of it and inject it in different component using dependency injection
+when ever an object is asked by each component.
+Now as the same object will be shared across so it will have same information across the project.Angular takes care of the
+injection.
+
+*/
+var ProductService = (function () {
+    function ProductService() {
+    }
+    ProductService.prototype.getProducts = function () {
+        return [
+            {
+                "productId": 1,
+                "productName": "Leaf Rake",
+                "productCode": "GDN-0011",
+                "releaseDate": "March 19, 2016",
+                "description": "Leaf rake with 48-inch wooden handle.",
+                "price": 19.95,
+                "starRating": 3.2,
+                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+            },
+            {
+                "productId": 2,
+                "productName": "Garden Cart",
+                "productCode": "GDN-0023",
+                "releaseDate": "March 18, 2016",
+                "description": "15 gallon capacity rolling garden cart",
+                "price": 32.99,
+                "starRating": 4.2,
+                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+            },
+            {
+                "productId": 5,
+                "productName": "Hammer",
+                "productCode": "TBX-0048",
+                "releaseDate": "May 21, 2016",
+                "description": "Curved claw steel hammer",
+                "price": 8.9,
+                "starRating": 4.8,
+                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+            },
+            {
+                "productId": 8,
+                "productName": "Saw",
+                "productCode": "TBX-0022",
+                "releaseDate": "May 15, 2016",
+                "description": "15-inch steel blade hand saw",
+                "price": 11.55,
+                "starRating": 3.7,
+                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+            },
+            {
+                "productId": 10,
+                "productName": "Video Game Controller",
+                "productCode": "GMG-0042",
+                "releaseDate": "October 15, 2015",
+                "description": "Standard two-button video game controller",
+                "price": 35.95,
+                "starRating": 4.6,
+                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+            }
+        ];
+    };
+    return ProductService;
+}());
+ProductService = __decorate([
+    core_1.Injectable()
+], ProductService);
+exports.ProductService = ProductService;
+//# sourceMappingURL=product.service.js.map
---
+++ app/products/product.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core'
+
+import { IProduct } from './product'
+
+/*
+In this class we are basically declaring the service which can be used in the product component anywhere.Similarly we can
+declaire the any other service such that it can be used across the product.Services can be such as logging service or 
+any other service where data need to be shared across different components.
+
+Here in ant component we can use these service by importing this class and then using it in that class 
+with 
+
+myservice m = new myservice();
+but this will create the individual object for every component and will data will not be shared across components.
+
+Anuglar provides the way out by defining the function which is @injectable() which hands this service to anguar and
+tells it that create a single object of it and inject it in different component using dependency injection
+when ever an object is asked by each component.
+Now as the same object will be shared across so it will have same information across the project.Angular takes care of the 
+injection.
+
+*/
+@Injectable()
+export class ProductService {
+
+    getProducts(): IProduct[]{
+        return [
+    {
+        "productId": 1,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2016",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    },
+    {
+        "productId": 2,
+        "productName": "Garden Cart",
+        "productCode": "GDN-0023",
+        "releaseDate": "March 18, 2016",
+        "description": "15 gallon capacity rolling garden cart",
+        "price": 32.99,
+        "starRating": 4.2,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+    },
+    {
+        "productId": 5,
+        "productName": "Hammer",
+        "productCode": "TBX-0048",
+        "releaseDate": "May 21, 2016",
+        "description": "Curved claw steel hammer",
+        "price": 8.9,
+        "starRating": 4.8,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+    },
+    {
+        "productId": 8,
+        "productName": "Saw",
+        "productCode": "TBX-0022",
+        "releaseDate": "May 15, 2016",
+        "description": "15-inch steel blade hand saw",
+        "price": 11.55,
+        "starRating": 3.7,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+    },
+    {
+        "productId": 10,
+        "productName": "Video Game Controller",
+        "productCode": "GMG-0042",
+        "releaseDate": "October 15, 2015",
+        "description": "Standard two-button video game controller",
+        "price": 35.95,
+        "starRating": 4.6,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+    }
+];
+}
+
+
+
+}
